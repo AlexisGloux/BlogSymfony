dde852fc88122b64bf2ddaf7c4eb504397b80b19
gloux <alexis.gloux@utt.fr>
2021-05-17 13:46:30 +0200

    comment + clean

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends AbstractController
 {
 
     /**
-     * @Route("/", name="app_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
