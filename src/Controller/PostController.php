e3d06b38269066cb2e4eaabf670fa3b373b52043
gloux <alexis.gloux@utt.fr>
2021-05-18 09:32:50 +0200

    Détails d'un article

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -18,4 +18,20 @@ class PostController extends AbstractController
             'posts' => $postRepository->findAll(),
         ]);
     }
+
+    /**
+     * @Route("/post/{id}", name="post_show", requirements={"id":"\d+"})
+     */
+    public function show(int $id, PostRepository $postRepository): Response
+    {
+        $post = $postRepository->findOneBy(['id' => $id]);
+
+        // Si l'id du post n'existe pas -> err 404
+        if (!$post)
+            throw $this->createNotFoundException('Article '.$id.' non trouvé');
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
 }
